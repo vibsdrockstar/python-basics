3b83b97f59e881c006873101277663f41c5d1243
Vibhor <bijoy.vibhor19@gmail.com>
2016-07-22 12:15:42 +0530

    Add files via upload

---
+++ sum and average.py
@@ -0,0 +1,12 @@
+Python 3.6.0a2 (v3.6.0a2:378893423552, Jun 14 2016, 01:21:40) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a=2
+>>> b=4
+>>> c=7
+>>> sum=a+b+c
+>>> print(sum)
+13
+>>> average=sum/3
+>>> print(average)
+4.333333333333333
+>>> 
